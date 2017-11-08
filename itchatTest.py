4e20bfd4f2d40efbd4ade57381bcf318075ed6d0
xiaofeng <xiao_feng@hunliji.com>
2017-11-08 16:57:34 +0800

    图灵聊天测试

--- itchatTest.py
+++ itchatTest.py
@@ -1,5 +1,6 @@
 #encoding:utf-8
 import itchat
+import requests
 from itchat.content import TEXT
 from itchat.content import *
 import sys
@@ -10,21 +11,41 @@ import os
 msg_information = {}
 face_bug = None
 
-'''
-    @todo 收到发送人的消息
-'''
+#自动把各种聊天内容发给微软小冰isXiaobingChat的小程序（附带图灵机器人和小冰聊天isTuling）
+isTuling,isXiaobingChat= True ,True
+def get_response(msg):
+    Url = "http://www.tuling123.com/openapi/api"
+    data = {
+        'key'  :  '603b728dce3340e4b0ac7f8cee17cb80', #这个key可以直接拿来用，随便用，无所谓，放心公开
+        'info'  : msg,
+        'userid' : 'pth-robot',
+    }
+    try:
+        r = requests.post(Url,data=data).json()
+        print("r1--------------")
+        print(r)
+        print("r2--------------------")
+        return r.get('text')
+    except:
+        return
+
+
 @itchat.msg_register(TEXT)
-def simple_reply(msg):
-    itchat.send(u'已经收到了文本消息，消息内容为%s'%msg['Text'],toUserName=msg['FromUserName'])
-    print(msg)
+def tuling_reply(msg):
+    reply = get_response(msg['text'])
+    return reply or 'I received: ' + msg.get('Text')
 
-'''
-    @todo 转发收到的消息至图灵机器人
-'''
+# 微信好友发来的内容isFriendChat=True, 群聊发来的内容isGroupChat=True, 公众号发来的内容isMpChat=False
+isFriendChat, isGroupChat , isMpChat=True,False,True
 
-'''
-    @todo 图灵机器人的返回的消息转发给发来人的
-'''
+
+
+@itchat.msg_register(TEXT,isFriendChat=isFriendChat, isGroupChat=isGroupChat, isMpChat=isMpChat)
+def reply_msg(msg):
+    itchat.send(tuling_reply(msg),toUserName=msg['FromUserName'])
+
+
+
 
 if __name__ == '__main__':
     itchat.auto_login(hotReload=True,enableCmdQR=2)
