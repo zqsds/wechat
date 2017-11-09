0ee7bc064e9171a7d5e64d6d22c53f17f044ef27
zqsds <1175860644@qq.com>
2017-11-09 23:54:46 +0800

    commit

--- kNN.py
+++ kNN.py
@@ -5,3 +5,7 @@ def createDataSet():
     group = array([[1.0,1.1],[1.0,1.0],[0,0],[0,0.1]])
     labels = ['A','A','B','B']
     return group,labels
+
+def classify(inX, dataSet, labels, k):
+    dataSetSize = dataSet.shape[0]
+    tile()
